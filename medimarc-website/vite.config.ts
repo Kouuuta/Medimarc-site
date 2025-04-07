7160d86f99b542322b892cff3f057966f4014fba
Kouuuta <koikeyuta357@gmail.com>
2025-04-07 14:53:01 +0800

    inital commit

--- medimarc-website/vite.config.ts
+++ medimarc-website/vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // Vite configuration
 export default defineConfig({
+  base: './',
   plugins: [react()],
   css: {
     postcss: './postcss.config.js',  // Reference PostCSS config file
